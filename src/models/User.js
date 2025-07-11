9eaf1ddc14c61c35ff5e22dcb78e01471c596745
Supreet <supreet7111999@gmail.com>
2025-07-11 16:29:40 +0530

    Added validator for email validations

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-
+const validator=require("validator");
 
 const userSchema=mongoose.Schema({
     firstName:{
@@ -17,7 +17,11 @@ const userSchema=mongoose.Schema({
         unique:true,
         lowercase: true,
         trim: true,
-
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid Email")
+            }
+        }
     },
     password:{
         type:String,
