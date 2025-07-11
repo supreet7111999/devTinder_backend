3efcd1ec186791ea8cea6fbc63d10616bf380eec
Supreet <supreet7111999@gmail.com>
2025-07-11 15:52:54 +0530

    Supreet|Some more in previous commit

--- src/app.js
+++ src/app.js
@@ -18,6 +18,37 @@ app.post("/signup",async(req,res)=>{
 })
 
 
+app.get("/user",async (req,res)=>{
+    const userEmail=req.bbody.emailId;
+    try{
+        const users=await User.find({emailId:userEmail});
+        if(users.length===0)
+            res.status(404).send("user not found");
+        else
+            res.send(users);
+    }
+    catch(err)
+    {
+        res.status(400).send("Something went wrong");
+    }
+
+})
+app.get("/alluser",async (req,res)=>{
+    // const userEmail=req.bbody.emailId;
+    try{
+        const users=await User.find({});
+        if(users.length===0)
+            res.status(404).send("user not found");
+        else
+            res.send(users);
+    }
+    catch(err)
+    {
+        res.status(400).send("Something went wrong");
+    }
+
+})
+
 
 
 connectDB()
