24aeed14ee0c11d3e55fa6cf3968c58e52d3b871
Supreet <supreet7111999@gmail.com>
2025-07-11 16:05:47 +0530

    Supreet|Added validations in UserSchema

--- src/models/User.js
+++ src/models/User.js
@@ -3,23 +3,49 @@ const mongoose=require("mongoose");
 
 const userSchema=mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required: true,
+        unique:true,
+        lowercase: true,
+        trim: true,
+
     },
     password:{
-        type:String
+        type:String,
+        required: true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value))
+                throw new Error("Gender is wrong");
+        },
+    },
+    photoUrl:{
+      type:String
+    },
+    about:{
+        type:String,
+        default:"Thiss is default value for about"
+    },
+    skills:{
+        type: [String]
     }
+},{
+    timestamps:true
 })
 
 const User=mongoose.model("User",userSchema);
